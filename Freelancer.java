809d85c7d7664daabe90673ad07bd06042a9ee26
alejandrolema25 <71530857+alejandrolema25@users.noreply.github.com>
2021-10-22 19:43:49 -0500

    Initial commit

---
+++ Freelancer.java
@@ -0,0 +1,17 @@
+package contaduriaSA;
+
+public class Freelancer extends Empleado{
+    private long valorHora;
+    private int horasTrabajadas;
+    public Freelancer(String nombre, long valorHora, int horasTrabajadas) {
+        super(nombre);
+        this.horasTrabajadas=horasTrabajadas;
+        this.valorHora=valorHora;
+    }
+
+    @Override
+    public long calcularSalario() {
+        return valorHora*horasTrabajadas;
+    }
+
+}
